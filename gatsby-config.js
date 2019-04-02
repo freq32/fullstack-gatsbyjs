9d4a14355f7686a358182d5a9d1ec6b4fa4cd9c1
Adi <developer@rosoftware.ro>
2019-04-02 19:30:14 +0300

    Update analytics trackingId [ appseed.us ]

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-google-analytics`,
 			options: {
-				trackingId: 'UA-XXXXXX-1',
+				trackingId: 'UA-104843706-1',
 				// Setting this parameter is optional (requried for some countries such as Germany)
 				anonymize: true
 			}
